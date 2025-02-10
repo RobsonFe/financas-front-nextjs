90f8c0dcb81d7c312c83f11a36d6cda83f951026
RobsonFe <robson_20ferreira@hotmail.com>
2025-02-09 23:18:46 -0300

    Remove div de altura mínima do layout para melhorar a estrutura da página

--- src/app/components/layout/index.tsx
+++ src/app/components/layout/index.tsx
@@ -25,7 +25,6 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
           </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
           <main>{children}</main>
         </div>
         <Footer />
